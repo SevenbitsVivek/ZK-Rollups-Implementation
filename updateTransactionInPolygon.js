d90b19988b088bcd7415b21ec2a2772f9d5340f1
SevenbitsVivek <vivek@sevenbits.in>
2023-04-10 15:18:06 +0530

    Resolved some bugs in updateTransactionInPolygon.js file

--- updateTransactionInPolygon.js
+++ updateTransactionInPolygon.js
@@ -6,25 +6,21 @@ const polygonAbi = require('./artifacts/contracts/UpdateTransaction.sol/UpdateTr
 
 const updateTransactionInPolygon = async (req, res) => {
     try {
-        var START_BLOCK
         const POLYGON_PROVIDER = new ethers.providers.JsonRpcProvider(process.env.POLYGON_PROVIDER);
         const polygonWallet = new ethers.Wallet(process.env.POLYGON_TESTNET_PRIVATE_KEY, POLYGON_PROVIDER);
         const contractInPolygon = new ethers.Contract(process.env.POLYGON_CONTRACT_ADDRESS, polygonAbi, polygonWallet);
         var processedTransaction = []
         fs.readFile("deposit.json", async function (err, data) {
             if (err) throw err;
-            const users = JSON.parse(data);
-            START_BLOCK = users
+            const START_BLOCK = JSON.parse(data);
             const latestBlockNumber = await POLYGON_PROVIDER.getBlockNumber();
             console.log("START_BLOCK ===>", START_BLOCK)
             console.log("latestBlockNumber ===>", latestBlockNumber)
             if (START_BLOCK < latestBlockNumber) {
                 const getBlockWithTransactions = await POLYGON_PROVIDER.getBlockWithTransactions(START_BLOCK)
-                processedTransaction.push("0xf3c41bc806e44af8d53455bec04e3c1fdf3761bea9d3d9c922b052d40fe8936b");
                 for (var i = 0; i < getBlockWithTransactions.transactions.length; i++) {
                     const hashData = getBlockWithTransactions.transactions[i].hash;
                     const transactionReceipt = await POLYGON_PROVIDER.getTransactionReceipt(hashData);
-                    console.log("!(processedTransaction.includes(hashData) ===>", !(processedTransaction.includes(hashData)))
                     if (transactionReceipt.status == 1 && !(processedTransaction.includes(hashData))) {
                         processedTransaction.push(hashData)
                     }
@@ -33,10 +29,11 @@ const updateTransactionInPolygon = async (req, res) => {
                 if (processedTransaction.length > 0) {
                     let processedTransactionsString = JSON.stringify(processedTransaction);
                     let encodeStringToBase64 = Buffer.from(processedTransactionsString).toString('base64');
-                    await fileHelper.setLatestBlock(START_BLOCK + 1);
                     console.log("encodeStringToBase64 ===>", encodeStringToBase64);
+                    processedTransaction.length = 0;
                     await contractInPolygon.setBase64(encodeStringToBase64);
                 }
+                await fileHelper.setLatestBlock(START_BLOCK + 1);
             }
         })
     } catch (error) {
