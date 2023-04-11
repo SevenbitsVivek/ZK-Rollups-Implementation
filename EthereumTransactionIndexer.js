ef412586b9736080206a496266d59ecaaf5a4261
SevenbitsVivek <vivek@sevenbits.in>
2023-04-11 20:07:33 +0530

    Made some changes in EthereumTransactionIndexer.js file

--- EthereumTransactionIndexer.js
+++ EthereumTransactionIndexer.js
@@ -1,9 +1,13 @@
 const ethers = require('ethers');
 const etherHelper = require('./helper/EtherHelper.js');
 const base64Helper = require('./helper/Base64Helper.js');
+const polygonAbi = require('./artifacts/contracts/UpdateTransaction.sol/UpdateTransaction.json')["abi"]
 const ethereumAbi = require('./artifacts/contracts/UpdateBase64Transaction.sol/UpdateBase64Transaction.json')["abi"]
+const POLYGON_PROVIDER = new ethers.providers.JsonRpcProvider(process.env.POLYGON_PROVIDER);
 const ETHEREUM_PROVIDER = new ethers.providers.JsonRpcProvider(process.env.ETHEREUM_PROVIDER);
+const polygonWallet = new ethers.Wallet(process.env.POLYGON_TESTNET_PRIVATE_KEY, POLYGON_PROVIDER);
 const ethereumWallet = new ethers.Wallet(process.env.GOERLI_TESTNET_PRIVATE_KEY, ETHEREUM_PROVIDER);
+const polygonContract = new ethers.Contract(process.env.POLYGON_CONTRACT_ADDRESS, polygonAbi, polygonWallet);
 const ethereumContract = new ethers.Contract(process.env.ETHEREUM_CONTRACT_ADDRESS, ethereumAbi, ethereumWallet);
 
 var EventData;
